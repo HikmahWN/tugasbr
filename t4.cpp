134c88084ff0c250c83214196385633e31c85b8c
HikmahWN <32409194+HikmahWN@users.noreply.github.com>
2017-10-24 22:10:59 +0800

    menampilkan luas 1 box parket

--- t4.cpp
+++ t4.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main(){
 	int luasRumah=42;
-	cout <<"Luas Rumah     :" <<luasRumah<<" m2"<<endl;
+	cout <<"Luas Rumah         :" <<luasRumah<<" m2"<<endl;
+	
+	float panjangParket=0.193;
+	float lebarParket=1.215;
+	float lp=panjangParket * lebarParket;
+	int b=8;
+	double luasBox=lp*8;
+	cout <<"Luas 1 box Parket  :" << luasBox <<" m2"<<endl;
 }
